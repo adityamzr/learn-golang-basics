1006f25546bd4e114d06b6c0242decd303811a4e
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2024-05-14 11:12:13 +0700

    feat(constant): Learn about Constant

---
+++ Constant/index.go
@@ -0,0 +1,16 @@
+package main
+
+func main()  {
+	// Constant satuan
+	const firstName string = "Nava"
+	const lastName = "Indriyanti"
+	
+	// Constant Multiple
+	const (
+		name = "Aditya"
+		age = 24
+		
+	)
+
+	// Constant tidak akan dikomplain oleh sistem jika tidak digunakan
+}
