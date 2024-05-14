fd1cf1eb955075eeac4e1004a50b1cc75c64ad04
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2024-05-14 11:11:24 +0700

    feat(variable): Learn about Variable

---
+++ Variable/index.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//Contoh variable yang didefinisikan tipe datanya
+	var name string
+
+	name = "Nava"
+	fmt.Println(name)
+	
+	name = "Nay"
+	fmt.Println(name)
+
+	//Contoh Variable yang tidak didefinisikan tipe datanya
+	var myName = "Aditya"
+	fmt.Println(myName)
+
+	// Secara default variable int akan bertipe data int saja
+	var age = 24
+	fmt.Println(age)
+
+	// Penggunaan var tidak wajib asalkan saat mendeklarasikan variable sudah dengan datanya
+	country := "Indonesia"
+	fmt.Println(country)
+
+	// variable bisa dibuat multiple
+	var (
+		firstName = "Aditya"
+		lastName = "Ramdani"
+	) 
+
+	fmt.Println(firstName, lastName)
+} 
