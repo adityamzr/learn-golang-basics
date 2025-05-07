8c192edcbcad85f9faec4687dca7de627a773ea7
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-05-07 20:01:52 +0700

    feat(conditional_statement): learn about else if statement

---
+++ Conditional Statement/else-if.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	gender := "oo"
+
+	if gender == "man" {
+		fmt.Println("Good you're a man!")
+	}else if gender == "woman" {
+		fmt.Println("Nice you're a woman!")
+	}else{
+		fmt.Println("YOU SON OF A B****!")
+	}
+}
