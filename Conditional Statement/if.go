c7d259cf92d29f7dc61b40f46219d14e290c6546
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-05-07 20:20:26 +0700

    fix(conditional_statement): merge all if statement to one file

--- Conditional Statement/if.go
+++ Conditional Statement/if.go
@@ -5,7 +5,31 @@ import "fmt"
 func main() {
 	name := "adid"
 
+	// IF STATEMENT
 	if name == "adid" {
 		fmt.Println(name)
+	}
+
+	// IF ELSE STATEMENT
+	if name == "adid" {
+		fmt.Println("Nama kamu adid")
+	}else {
+		fmt.Println("Siapa nama kamu?")
+	}
+
+	// ELSE IF STATEMENT
+	if name == "adid" {
+		fmt.Println("Hi adid!")
+	}else if name == "zain" {
+		fmt.Println("Hi zain!")
+	}else {
+		fmt.Println("Nama kamu siapa?")
+	}
+
+	// SHORTEN IF STATEMENT
+	if length := len(name); length > 5 {
+		fmt.Println("Nama kamu panjang ya")
+	}else {
+		fmt.Println("Nama kamu pendek tapi bagus")
 	}
 }
